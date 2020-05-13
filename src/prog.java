c541c14e36cafee4812620424f91a875073e3c2c
Davizera-png <olidavidson34@gmail.com>
2020-05-13 19:46:13 -0300

    Leitura de escolaridade

--- src/prog.java
+++ src/prog.java
@@ -1,8 +1,17 @@
-
+import java.util.Scanner;
 public class prog {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int escolaridade;
 		
+		System.out.println("Qual a sua escolaridade");
+		System.out.println("1) Ensino fundamental");
+		System.out.println("2) Ensino médio");
+		System.out.println("3) Ensino superior");
+		System.out.println("4) Pós-graduação");
+		System.out.printf("Digite uma opção: ");
+		escolaridade = sc.nextInt();
 
 	}
 
