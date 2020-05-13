7b0b8cc88035af3de64a39ad5da221567f03cb80
Davizera-png <olidavidson34@gmail.com>
2020-05-13 20:06:06 -0300

    Leitura dos outros dados

--- src/prog.java
+++ src/prog.java
@@ -3,7 +3,8 @@ public class prog {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		int escolaridade;
+		int escolaridade, exp;
+		char opc1, opc2;
 		
 		System.out.println("Qual a sua escolaridade");
 		System.out.println("1) Ensino fundamental");
@@ -12,6 +13,14 @@ public class prog {
 		System.out.println("4) Pós-graduação");
 		System.out.printf("Digite uma opção: ");
 		escolaridade = sc.nextInt();
+		
+		System.out.printf("Você tem quantos anos de experiência profissional ? ");
+		exp = sc.nextInt();
+		System.out.printf("Você tem disponibilidade para viajar (S/N) ? ");
+		opc1 = sc.next().charAt(0);
+		System.out.printf("Você tem habilitação de motorista (S/N) ? ");
+		opc2 = sc.next().charAt(0);
+		
 
 	}
 
