8fe03528417de5ab1612d56f850e1c610e7216ed
Davizera-png <olidavidson34@gmail.com>
2020-05-13 20:41:06 -0300

    Escolaridade e experiência

--- src/prog.java
+++ src/prog.java
@@ -3,9 +3,11 @@ public class prog {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		int escolaridade, exp;
+		int escolaridade, exp, pesc, pexp;
 		char opc1, opc2;
-		
+		pesc = 0;
+		pexp = 0;
+		//leitura de dados
 		System.out.println("Qual a sua escolaridade");
 		System.out.println("1) Ensino fundamental");
 		System.out.println("2) Ensino médio");
@@ -21,7 +23,32 @@ public class prog {
 		System.out.printf("Você tem habilitação de motorista (S/N) ? ");
 		opc2 = sc.next().charAt(0);
 		
-
+		//calculos
+		
+		//estrutura de escolaridade
+		if (escolaridade == 1) {
+			pesc = pesc + 10;
+		} else if (escolaridade == 2) {
+			pesc = pesc + 20;
+		} else if (escolaridade == 3) {
+			pesc = pesc + 30;
+		} else if (escolaridade == 4) {
+			pesc = pesc + 40;
+		}
+		
+		//estrutura de experiencia
+		if (exp > 0 && exp < 2) {
+			pexp = pexp + 10;
+		} else if (exp > 2 && exp < 5) {
+			pexp = pexp + 20;
+		} else if (exp > 5) {
+			pexp = pexp + 40;
+		}
+		//termino de calculos
+		 System.out.println("Pontos de escolaridade"+ pesc);
+		 System.out.println("Pontos de experiência"+ pexp);
+		
+		
 	}
 
 }
